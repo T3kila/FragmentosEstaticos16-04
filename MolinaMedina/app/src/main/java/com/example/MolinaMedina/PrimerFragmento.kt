c180949bf7936ce3b0a0855fbb59b615dd64039c
T3kila <diegoalfmolina@gmail.com>
2024-04-25 18:35:50 -0600

    Fragmento Estaticos 16.09

---
+++ MolinaMedina/app/src/main/java/com/example/MolinaMedina/PrimerFragmento.kt
@@ -0,0 +1,44 @@
+package com.example.MolinaMedina
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+
+
+class PrimerFragmento : Fragment() {
+
+    private lateinit var editText: EditText
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_primer_fragmento, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val boton = view.findViewById<Button>(R.id.btn_boton)
+        editText = view.findViewById(R.id.txt_fragmento1)
+
+        boton.setOnClickListener({view ->
+
+            var message : String = editText.text.toString()
+            val bundle = Bundle()
+            bundle.putString("Valor_campo",message)
+
+            arguments = bundle
+
+            parentFragmentManager.setFragmentResult("key",bundle)
+
+        })
+
+    }
+
+}
